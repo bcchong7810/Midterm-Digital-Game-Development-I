95d47da54da382146cafd1540af71e7f36a054bd
bcchong7810 <bcchong7810@gmail.com>
2026-03-23 07:46:15 -0400

    Changed some variables, broke it and then fixed it?
    SR name changes

--- Assets/Scripts/HorizontalHazardScript.cs
+++ Assets/Scripts/HorizontalHazardScript.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class HorizontalHazardScript : MonoBehaviour
 {
-    public Rigidbody2D RB = new Rigidbody2D();
+    public Rigidbody2D HHRB = new Rigidbody2D();
     public Vector2 velocity = new Vector2(0.0f, 0.0f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,19 +14,19 @@ public class HorizontalHazardScript : MonoBehaviour
     void Update()
     {
         
-        if (RB.linearVelocity.x >= 0.0f && RB.position.x >= 4.5f)
+        if (HHRB.linearVelocity.x >= 0.0f && HHRB.position.x >= 4.5f)
         { 
             velocity.x = -5.0f;
             velocity.y = 0.0f; 
         }
 
-        if (RB.linearVelocity.x <= 0.0f && RB.position.x <= -4.5f)
+        if (HHRB.linearVelocity.x <= 0.0f && HHRB.position.x <= -4.5f)
         {
             velocity.x = 5.0f;
             velocity.y = 0.0F; 
         }
 
-        RB.linearVelocity = velocity;
+        HHRB.linearVelocity = velocity;
 
 
 
--- Assets/Scripts/VerticalHazardScript.cs
+++ Assets/Scripts/VerticalHazardScript.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class HazardScript : MonoBehaviour
 {
-    public Rigidbody2D RB = new Rigidbody2D();
+    public Rigidbody2D VHRB = new Rigidbody2D();
     public Vector2 velocity = new Vector2(0.0f, 0.0f);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,19 +16,19 @@ public class HazardScript : MonoBehaviour
     {   
 
         /*Vertical patrol path for hazards/lava below*/
-        if (RB.linearVelocity.y >= 0.0f && RB.position.y >= 4.5f)
+        if (VHRB.linearVelocity.y >= 0.0f && VHRB.position.y >= 4.5f)
         { 
             velocity.x = 0.0f;
             velocity.y = -5.0f; //Moves down after hitting upper limit of patrol
         }
 
-        if (RB.linearVelocity.y <= 0.0f && RB.position.y <= -4.5f)
+        if (VHRB.linearVelocity.y <= 0.0f && VHRB.position.y <= -4.5f)
         {
             velocity.x = 0.0f;
             velocity.y = 5.0F; //Moves up after hitting lower limit of patrol
         }
 
-        RB.linearVelocity = velocity;
+        VHRB.linearVelocity = velocity;
 
 
 
