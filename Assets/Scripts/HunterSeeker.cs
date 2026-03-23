24e50022f6bc67b003af08230ba9d731ee2e9b27
bcchong7810 <bcchong7810@gmail.com>
2026-03-23 08:22:32 -0400

    1 Hunter Seeker template works

--- Assets/Scripts/HunterSeeker.cs
+++ Assets/Scripts/HunterSeeker.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using UnityEngine.Jobs;
 
 public class HunterSeeker : MonoBehaviour
 {
+    public Rigidbody2D RB;
+    public Rigidbody2D PLRB;
+    public Vector2 velocity;
+    public Vector2 playerPosition;
+    public Vector2 hunterSeekerPosition;
+    public static float distance;
+    public float aggro = 3.0f;
+    public SpriteRenderer SR;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,38 @@ public class HunterSeeker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerPosition = PLRB.position;
+        hunterSeekerPosition = RB.position;
+        distance = Vector2.Distance(playerPosition, hunterSeekerPosition);
         
+        if (distance <= aggro)
+        {
+            SR.color = Color.deepPink;
+            if (playerPosition.x > hunterSeekerPosition.x)
+            {
+                velocity.x = 3.0f;
+            } else if (playerPosition.x < hunterSeekerPosition.x)
+            {
+                velocity.x = -3.0f;
+            }
+
+            if (playerPosition.y > hunterSeekerPosition.y)
+            {
+                velocity.y = 3.0f;
+            } else if (playerPosition.y < hunterSeekerPosition.y)
+            {
+                velocity.y = -3.0f;
+            } 
+        }
+        else
+        {
+            velocity.x = 0.0f;
+            velocity.y = 0.0f;
+        }
+
+
+        RB.linearVelocity = velocity;
+
+
     }
 }
